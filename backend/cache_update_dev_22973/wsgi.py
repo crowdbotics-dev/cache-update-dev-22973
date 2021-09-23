f258c4f1b1363ccd91238f58b2320ed7c53a62bd
Crowdbotics <engineering@crowdbotics.com>
2021-09-23 19:26:54 +0000

    Initial commit.

---
+++ backend/cache_update_dev_22973/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cache_update_dev_22973 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cache_update_dev_22973.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cache_update_dev_22973",
+  url: "https://cache_update_dev_22973.botics.co",
+  api: "https://cache_update_dev_22973.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
